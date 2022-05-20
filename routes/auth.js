e1acfeb6c1f15afb1d54987a58010168fed36769
amichalo421 <andre.michalowski@gmail.com>
2022-05-20 10:52:43 -0400

    Save user in db

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,10 @@ import express from "express";
 const router = express.Router();
 
 //controllers
-import {showMessage, register} from "../controllers/auth"
+import { register } from "../controllers/auth";
+import { showMessage } from "../controllers/showMessage";
 
 router.get("/:message", showMessage);
-router.post('/register', register);
+router.post("/register", register);
 
 module.exports = router;
